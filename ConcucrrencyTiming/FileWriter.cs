065e876e5bf28d29bfa160933d72f60f590439ea
Matt Welch <mattw0308@gmail.com>
2014-11-29 23:22:33 -0700

    now include file writer

---
+++ ConcucrrencyTiming/FileWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConcucrrencyTiming
+{
+    class FileWriter
+    {
+        public FileWriter(string filename, List<long> data)
+        {
+            string[] strArray = new string[data.Count];
+            for (int i=0; i < data.Count; i++)
+            {
+                strArray[i] = data[i].ToString();
+            }
+            string path = Directory.GetCurrentDirectory();
+            File.WriteAllLines(filename, strArray);
+        }
+    }
+}
